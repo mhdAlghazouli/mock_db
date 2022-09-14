b2fa5af3c4edafd6600bffed6cfd11e95db4880d
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-09-14 13:34:02 -0400

    Added api.js

---
+++ JS/api.js
@@ -0,0 +1,37 @@
+const heroes = [
+  {
+    name : 'Super Man',
+    alter_ego : 'Clark Kent',
+    universe : 'Earth Q',
+    powers : ['Invulnerability','Super Speed','Flight'],
+    arch_enemy : 'Lex Luthor'
+  }, 
+  {
+    name : 'Hulk',
+    alter_ego : 'Bruce Banner',
+    universe : 'Marvel',
+    powers :['Transformation','Control','Thunderclap'],
+    arch_enemy : 'The Leader'
+  },
+  {
+    name : 'Captain America',
+    alter_ego : 'Steve Rogers',
+    universe : 'Marvel',
+    powers :['Superhuman Agility','Superhuman Stamina','Superhuman Durability'],
+    arch_enemy : 'The Red Skull'
+  },
+  {
+    name : 'Iron Man',
+    alter_ego : 'Tony Stark',
+    universe : 'Marvel',
+    powers :['superhuman strength','durability','flight'],
+    arch_enemy : 'Mandarin'
+  },
+  {
+    name : 'Thor',
+    alter_ego : 'Thor',
+    universe : 'Marvel',
+    powers : ['enhanced longevity','speed','superhuman strength'],
+    arch_enemy : 'Loki'
+  }
+]
